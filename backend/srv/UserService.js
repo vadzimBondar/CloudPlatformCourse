4cc9046b15cb8edc60321a3ee7678cbeced4b213
vadzimBondar <vadzim.bondar@leverx.com>
2019-12-26 14:30:15 +0300

    Add mta files

---
+++ backend/srv/UserService.js
@@ -0,0 +1,30 @@
+//MOCK service
+module.exports = (srv) => {
+
+    srv.on('READ', 'Users', () => [
+        {
+            usid: 'U001', name: "Batman",
+            toCars: [
+                {crid: "C001", usid: "U001", name: "BatMobile", toUser: {usid: 'U001', name: "Batman"}}
+            ],
+            toAddress: [
+                { adid: "A001", usid: "U001", city: "Gotam", strt: "unknown" }
+            ]
+        }
+    ]);
+
+    srv.on('READ', 'Address', () => [
+        { adid: "A001", usid: "U001", city: "Gotam", strt: "unknown" }
+    ]);
+
+    srv.on('READ', 'Cars', () => [
+        {crid: "C001", usid: "U001", name: "BatMobile", toUser: {usid: 'U001', name: "Batman"}}
+    ]);
+
+srv.on('READ', 'Books', () => [
+	{
+		bsid: "U001", name: "Some book", pages: "1234"
+	}
+]);
+
+};
